85aa73a288582c5ce3e857165a1338dda8817163
forgiveboo <wyx5051@gmail.com>
2023-01-05 04:54:32 +0800

    add some plugin

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -8,7 +8,17 @@ require("mason").setup({
     }
 })
 
+require'cmp'.setup {
+  sources = {
+    { name = 'nvim_lsp' }
+  }
+}
+
+-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 require'lspconfig'.pyright.setup{
 	on_attach = on_attach,
   flags = lsp_flags,
+  capabilities = capabilities,
 }
