c8404d3dd6ab85f7eedfca7f2f79583c48302f86
forgiveboo <wyx5051@gmail.com>
2023-01-05 20:25:30 +0800

    update lsp

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -14,21 +14,14 @@ bufferline.setup {
       text = "File Explorer",
       highlight = "Directory",
       text_align = "left"
-    }}
+    }},
+		diagnostics_indicator = function(count, level, diagnostics_dict, context)
+		local icon = level:match("error") and " " or " "
+		return " " .. icon .. count
+		end
   }
 }
 
 --按键映射
 --nnoremap <silent> gb :BufferLinePick<CR>
 vim.api.nvim_set_keymap("n", "gb", "<Cmd>BufferLinePick<CR>", {noremap = true, silent = true})
-
-vim.api.nvim_set_keymap("n", "<leader>1", "<Cmd>BufferLineGoToBuffer 1<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>2", "<Cmd>BufferLineGoToBuffer 2<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>3", "<Cmd>BufferLineGoToBuffer 3<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>4", "<Cmd>BufferLineGoToBuffer 4<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>5", "<Cmd>BufferLineGoToBuffer 5<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>6", "<Cmd>BufferLineGoToBuffer 6<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>7", "<Cmd>BufferLineGoToBuffer 7<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>8", "<Cmd>BufferLineGoToBuffer 8<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>9", "<Cmd>BufferLineGoToBuffer 9<CR>", {noremap = true, silent = true})
-
--- lua/tools/keymaps.lua
+++ lua/tools/keymaps.lua
@@ -11,6 +11,17 @@ local opt = {
 
 map('n', '<A-m>', ':NvimTreeToggle<CR>', opt)
 
+-- alt + hjkl  窗口之间跳转
+map("n", "<A-h>", "<C-w>h", opt)
+map("n", "<A-j>", "<C-w>j", opt)
+map("n", "<A-k>", "<C-w>k", opt)
+map("n", "<A-l>", "<C-w>l", opt)
+
+-- bufferline 左右Tab切换
+map("n", "<C-h>", ":BufferLineCyclePrev<CR>", opt)
+map("n", "<C-l>", ":BufferLineCycleNext<CR>", opt)
+map("n", "<C-w>", ":bd<CR>", opt)
+
 local pluginKeys = {}
 
 -- lsp 回调函数快捷键设置
